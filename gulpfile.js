23572e35dae5780ac2380b4a30a002f20a8d1a4a
mwarnsley <marcus.warnsley@gmail.com>
2016-01-04 20:42:38 -0500

    Getting started using Gulp as a task runner

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+//Gulp.js configuration
+
+//Include gulp and plugins
+
+var gulp = require('gulp');
+
+var source = 'source/'.
+	dest = 'build/';
+
+//default task
+
+gulp.task('default', function(){
+
+});
