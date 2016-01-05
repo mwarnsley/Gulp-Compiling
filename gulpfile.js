95b08f6e147d2a2d1d6b014ddcfa05a01054f7d4
mwarnsley <marcus.warnsley@gmail.com>
2016-01-04 21:17:52 -0500

    Adding gulp newer plugin to process only changed images when compressing

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@
 //Include gulp and plugins
 
 var gulp = require('gulp'),
-	imagemin = require('gulp-imagemin');
+	imagemin = require('gulp-imagemin'),
+	newer = require('gulp-newer');
 
 var source = 'source/',
 	dest = 'build/',
@@ -17,6 +18,7 @@ var source = 'source/',
 
 gulp.task('images', function(){
 	return gulp.src(images.in)
+		.pipe(newer(images.out))
 		.pipe(imagemin())
 		.pipe(gulp.dest(images.out));
 });
